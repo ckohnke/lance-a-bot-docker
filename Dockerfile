86acee3ccabb114608267828fb64e50de5c978b9
Colton Kohnke <colton.kohnke@gmail.com>
2019-10-28 22:09:22 -0600

    appdata working now

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM python:3
 
+WORKDIR /usr/src/lance-a-bot
+
 COPY requirements.txt ./
 RUN apt-get update && apt-get -y install libgeos-dev 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY lance.py /app/lance.py
+COPY lance.py .
 
-CMD [ "python", "/app/lance.py" ]
+VOLUME /config
+
+#CMD [ "python", "lance.py" ]
--- lance.py
+++ lance.py
@@ -24,6 +24,8 @@ TOKEN = os.environ['GEOS_TOKEN']
 USERNAME = os.environ['POP_UNAME']
 PASSWORD = os.environ['POP_PASSWORD']
 
+PWD = "/config/"
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
@@ -42,8 +44,8 @@ creds = None
 # The file token.pickle stores the user's access and refresh tokens, and is
 # created automatically when the authorization flow completes for the first
 # time.
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
+if os.path.exists(PWD+'token.pickle'):
+    with open(PWD+'token.pickle', 'rb') as token:
         creds = pickle.load(token)
 # If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
@@ -51,10 +53,10 @@ if not creds or not creds.valid:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
+            PWD+'credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
+    with open(PWD+'token.pickle', 'wb') as token:
         pickle.dump(creds, token)
 
 service = build('calendar', 'v3', credentials=creds)
